de83e1dd9ea230ef0cbbdd3afabd311a3417ee3a
MaksimBlud <kuzkinma@gmail.com>
2026-09-05 03:12:09 -0400

    Test Turkey and Portugal dedicated operational readiness

--- tests/test_turkey_portugal_league_config.py
+++ tests/test_turkey_portugal_league_config.py
@@ -7,20 +7,20 @@ from league_config import (
 )
 
 
-def test_turkey_super_lig_is_registered_but_inert():
+def test_turkey_super_lig_uses_dedicated_operational_collection_only():
     assert TURKEY_SUPER_LIG.identifier == "TURKEY_SUPER_LIG"
     assert TURKEY_SUPER_LIG.odds_api_sport_key == "soccer_turkey_super_league"
     assert TURKEY_SUPER_LIG.timezone == "Europe/Istanbul"
     assert not is_collection_ready("TURKEY_SUPER_LIG")
-    assert not is_operational_collection_ready("TURKEY_SUPER_LIG")
+    assert is_operational_collection_ready("TURKEY_SUPER_LIG")
 
 
-def test_primeira_liga_is_registered_but_inert():
+def test_primeira_liga_uses_dedicated_operational_collection_only():
     assert PRIMEIRA_LIGA.identifier == "PRIMEIRA_LIGA"
     assert PRIMEIRA_LIGA.odds_api_sport_key == "soccer_portugal_primeira_liga"
     assert PRIMEIRA_LIGA.timezone == "Europe/Lisbon"
     assert not is_collection_ready("PRIMEIRA_LIGA")
-    assert not is_operational_collection_ready("PRIMEIRA_LIGA")
+    assert is_operational_collection_ready("PRIMEIRA_LIGA")
 
 
 def test_configured_leagues_contains_nine_unique_identifiers():
